4cf3eca38a2d2be8f7727397c6dd40b01d4561c0
Thirza Rep <thirza-rep@users.noreply.github.com>
2026-04-14 21:31:36 +0700

    feat: integrate L5-Swagger for API documentation and configure OpenAPI specification

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="ConnectX API Documentation",
+ *      description="Auto-generated API docs untuk ConnectX Project",
+ *      @OA\Contact(
+ *          email="developer@connectx.app"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * 
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Local Demo API Server"
+ * )
+ */
 abstract class Controller
 {
     //
