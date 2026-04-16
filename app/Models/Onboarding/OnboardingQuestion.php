d30ce305a63f90255597bc770e21cca3ef582647
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-04-17 03:46:53 +0700

    fix(models): add HasUuids to all Onboarding models to auto-generate UUID on create

--- app/Models/Onboarding/OnboardingQuestion.php
+++ app/Models/Onboarding/OnboardingQuestion.php
@@ -2,12 +2,13 @@
 
 namespace App\Models\Onboarding;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OnboardingQuestion extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'onboarding_questions';
 
