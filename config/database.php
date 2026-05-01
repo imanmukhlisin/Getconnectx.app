a266eaa05150c38e5626e1faafdaa6d2b071b642
Dimas Oktavian Prasetyo <dimasoktavianprasetyo@hotmail.com>
2026-05-01 13:15:49 +0700

    feat: add PDO emulation option to PostgreSQL configuration for PgBouncer compatibility

--- config/database.php
+++ config/database.php
@@ -131,6 +131,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => env('PGBOUNCER', false) ? [
+                \PDO::ATTR_EMULATE_PREPARES => true,
+            ] : [],
         ],
 
         'sqlsrv' => [
