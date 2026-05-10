a2d68cc3792574595f59633da161ab545332db7e
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-05-10 07:04:44 +0700

    fix: override storage path in bootstrap/app.php for Vercel deployment to write to /tmp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -29,3 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
     })->create();
+
+if (isset($_ENV['APP_STORAGE_PATH'])) {
+    $app->useStoragePath($_ENV['APP_STORAGE_PATH']);
+}
+
+return $app;
