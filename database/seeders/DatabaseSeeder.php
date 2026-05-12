6dfda5c4445ee393be84c88643a09fc2bf347fa5
Dimas Oktavian Prasetyo <dimasoktavianprasetyo@hotmail.com>
2026-05-13 01:56:05 +0700

    feat: add AdminSeeder to initialize default admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,
             TagSeeder::class,
             OnboardingSeeder::class,
             NotificationTemplateSeeder::class,
